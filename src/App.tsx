41101439b5c1894b95580f10c72c190b66e0bbbf
jahanabir8 <onupomabir@gmail.com>
2026-09-07 17:47:35 +0300

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Nav from './Components/Nav/Nav'
 
 function App() {
 
   return (
     <>
-    
+      <Nav></Nav>
     </>
   )
 }
---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import Coin from "../../assets/coin.png";
+import Logo from '../../assets/logo.png'
+
+const Nav = () => {
+  return (
+    <div className=" shadow-sm">
+      <div className="navbar max-w-330 mx-auto bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                aria-label="Menu"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Fixtures</a>
+              </li>
+              <li>
+                <a>Teams</a>
+              </li>
+              <li>
+                <a>Schedules</a>
+              </li>
+            </ul>
+          </div>
+          <a className="cursor-pointer">
+            <img src={Logo} alt="" />
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Fixtures</a>
+            </li>
+            <li>
+              <a>Teams</a>
+            </li>
+            <li>
+              <a>Schedules</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">
+            Coin <img className="h-5" src={Coin} alt="coin" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
